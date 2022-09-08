ac7b76daada4522b80795f5376d07fd012a4fd30
thi-phng <thi-phng@student.42.fr>
2022-09-08 18:27:42 +0200

    updated run_me.sh on vim and trying to update docker

--- run_me.sh
+++ run_me.sh
@@ -10,4 +10,5 @@ sudo apt-get install zsh -y
 #install docker
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
