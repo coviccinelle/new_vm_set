3d8ffd5ba160759e75070bc5c656fbc4ee9e8eeb
thi-phng <thi-phng@student.42.fr>
2022-09-08 18:26:55 +0200

    updated run_me.sh on vim and trying to update docker

---
+++ run_me.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade
+sudo apt-get update
+sudo apt-get install vim
+
+sudo apt-get install zsh -y
+
+#install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
